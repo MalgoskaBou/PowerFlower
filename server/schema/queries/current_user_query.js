84691a1241b4aec1682ff736c69d81a905c17ed9
MalgoskaG <ghouga.m@gmail.com>
2020-01-06 21:36:51 +0100

    Current user query

---
+++ server/schema/queries/current_user_query.js
@@ -0,0 +1,13 @@
+const UserType = require("../types/user_type");
+const User = require("../../models/user");
+const { authorization } = require("../../services/auth");
+
+const currentUser = {
+  type: UserType,
+  resolve(parentValue, args, req) {
+    authorization(req);
+    return User.findById(req.user.id);
+  }
+};
+
+module.exports = currentUser;
--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -4,6 +4,7 @@ const flowers = require("./queries/flowers_query");
 const flower = require("./queries/flower_query");
 const zones = require("./queries/zones_query");
 const zone = require("./queries/zone_query");
+const currentUser = require("./queries/current_user_query");
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
@@ -11,7 +12,8 @@ const RootQueryType = new GraphQLObjectType({
     flowers,
     zones,
     flower,
-    zone
+    zone,
+    currentUser
   }
 });
 
