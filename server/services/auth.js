b5204cb6d56d6eaa367076d3cb7b9ff1eb9bb731
MalgoskaG <ghouga.m@gmail.com>
2020-02-01 19:20:26 +0100

    Delete unnecessary validation in auth (model is validated)

--- server/services/auth.js
+++ server/services/auth.js
@@ -5,9 +5,6 @@ const confirmLinkEmailTemplate = require("./confirmLinkEmailTemplate");
 
 function signup({ email, password, name }) {
   const user = new User({ email, password, name });
-  if (!email || !password) {
-    throw new Error("You must provide an email and password.");
-  }
 
   return User.findOne({ email }).then(existingUser => {
     if (existingUser) {
