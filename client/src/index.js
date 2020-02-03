8654be83f4d7dec96db259ca5cdedd0d37fa1db2
MalgoskaG <ghouga.m@gmail.com>
2020-02-03 23:29:10 +0100

    Add router and first query - currentUser

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import LoginForm from "./components/LoginForm";
+import Dashboard from "./components/Dashboard";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  dataIdFromObject: object => object.key || null
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact path="/">
+          <LoginForm />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
