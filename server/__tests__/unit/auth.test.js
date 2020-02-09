52d4d64e5f683444bf6fb99644f3c199d4128d81
MalgoskaG <ghouga.m@gmail.com>
2020-02-09 19:38:20 +0100

    Unit tests

--- server/__tests__/unit/auth.test.js
+++ server/__tests__/unit/auth.test.js
@@ -1,24 +1,35 @@
 const Auth = require("../../services/auth");
 
-describe("authorization", () => {
-  it("there is no user logged", () => {
+describe("User authorization", () => {
+  it("User is not logged in", () => {
     const req = {
       user: null
     };
-    expect(() => Auth.authorization(req)).toThrowError(
-      "You must be logged in!"
-    );
+    expect(() => Auth.authorization(req)).toThrowError(/logged in!/);
   });
 
-  it("user didn't confirm email", () => {
+  it("User didn't confirm email", () => {
     const req = {
       user: {
-        name: "kitek",
         confirmed: false
       }
     };
-    expect(() => Auth.authorization(req)).toThrowError(
-      "You must confirm your account to use application!"
-    );
+    expect(() => Auth.authorization(req)).toThrowError(/confirm/);
+  });
+});
+
+describe("Service auth func", () => {
+  it("sendMailWIthConfirmLink", () => {
+    const req = {
+      headers: {
+        host: "http://host.com/"
+      }
+    };
+    const user = {
+      _id: "1",
+      email: "email@email.com"
+    };
+    const emailContent = Auth.createConfirmationMail(user, req);
+    expect(emailContent).toMatch(/confirm\/1/);
   });
 });
