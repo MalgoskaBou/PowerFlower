48fda41b16ea240d2459ae40c254e4f3ac205b67
MalgoskaG <ghouga.m@gmail.com>
2020-02-11 22:23:39 +0100

    Repair supertest with query and mutation

--- server/__tests__/integration/query.test.js
+++ server/__tests__/integration/query.test.js
@@ -1,32 +1,39 @@
 const request = require("supertest");
 
-let server, agent;
-const graphQLURL = "http://localhost:4000/graphql";
+let server;
+
+beforeEach(() => {
+  server = require("../../index");
+});
+afterEach(async () => {
+  await server.close();
+});
+afterAll(async () => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+  // avoid jest open handle error
+});
 
 describe("GraphQL queries", () => {
-  beforeEach(() => {
-    server = require("../../index");
-  });
-  afterEach(async () => {
-    await server.close();
-  });
-  afterAll(async () => {
-    await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
-  });
-
   it("User is not logged in", async () => {
     const res = await request(server)
-      .post(graphQLURL)
+      .post("/graphql")
       .send({ query: "{ currentUser { id } }" });
-
-    expect(res.status).toBe(403);
+    expect(res.status).toBe(200);
   });
 
-  it("User is not logged in", async () => {
+  it("Login user", async () => {
     const res = await request(server)
-      .post(graphQLURL)
-      .send({ query: "{ currentUser { id } }" });
-
-    expect(res.status).toBe(403);
+      .post("/graphql")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .send({
+        query: `mutation {
+  loginUser(email:"email5@email.com", password: "dupa"){
+    email
+    name
+  }
+}`
+      });
+    expect(res.status).toBe(200);
   });
 });
