3c0e600ddbfcb65d6ddfe703a716670e94e2cc1a
MalgoskaG <ghouga.m@gmail.com>
2020-02-12 21:54:05 +0100

    simple login page styling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,68 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../img/loginFlower.svg";
+
+const Wrapper = styled.section`
+  height: 100vh;
+  width: 100 wh;
+  background: linear-gradient(125.61deg, #3f5169 0.46%, #202e45 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background-color: #e8988c;
+  padding: 1rem 0px;
+  border: none;
+  border-radius: 3rem;
+  color: white;
+  font-size: 1.8rem;
+  letter-spacing: 0.5em;
+  width: 50%;
+  max-width: 300px;
+  text-transform: uppercase;
+  font-weight: lighter;
+`;
+
+const Input = styled.input`
+  background-color: transparent;
+  border: none;
+  height: 3rem;
+  width: 50%;
+  margin: 3rem 0;
+  border-bottom: 0.5px solid white;
+  max-width: 300px;
+  font-size: 2.4rem;
+  color: white;
+  font-weight: lighter;
+  padding-left: 50px;
+
+  &::placeholder {
+    color: white;
+  }
+`;
+
+const CustomLink = styled.a`
+  color: #ffffff70;
+  font-size: 1.3rem;
+  margin: 1rem;
+  font-weight: lighter;
+`;
 
 const LoginForm = () => {
-  return <div>Login Form</div>;
+  return (
+    <Wrapper>
+      <img src={logo} />
+      <Input type="email" name="email" placeholder="e-mail" />
+      <Input type="password" name="password" placeholder="Password" />
+      <Button type="submit" value="Wyślij">
+        Log in
+      </Button>
+      <CustomLink>Forgot password</CustomLink>
+    </Wrapper>
+  );
 };
 
 export default LoginForm;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import "./index.css";
 
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./components/Dashboard";
