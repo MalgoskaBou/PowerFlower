bdf5e9ef03415c4d21713048422bbf3f60a66f16
MalgoskaG <ghouga.m@gmail.com>
2020-02-13 23:04:09 +0100

    next dummy styling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../img/loginFlower.svg";
+import iconMail from "../img/icon_mail.svg";
+import iconPassword from "../img/icon_password.svg";
 
 const Wrapper = styled.section`
   height: 100vh;
@@ -10,11 +12,17 @@ const Wrapper = styled.section`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  input:first-of-type {
+    margin-top: 10rem;
+  }
+  input:last-of-type {
+    margin-bottom: 5rem;
+  }
 `;
 
 const Button = styled.button`
   background-color: #e8988c;
-  padding: 1rem 0px;
+  padding: 0.7rem 0px;
   border: none;
   border-radius: 3rem;
   color: white;
@@ -31,14 +39,16 @@ const Input = styled.input`
   border: none;
   height: 3rem;
   width: 50%;
-  margin: 3rem 0;
-  border-bottom: 0.5px solid white;
+  margin: 2rem 0;
+  border-bottom: 0.5px solid #ffffff70;
   max-width: 300px;
   font-size: 2.4rem;
   color: white;
   font-weight: lighter;
-  padding-left: 50px;
-
+  padding: 0 0 8px 50px;
+  background: url(${props =>
+      props.type === "email" ? iconMail : iconPassword})
+    no-repeat;
   &::placeholder {
     color: white;
   }
@@ -54,9 +64,11 @@ const CustomLink = styled.a`
 const LoginForm = () => {
   return (
     <Wrapper>
-      <img src={logo} />
+      <img src={logo} style={{ maxWidth: "150px" }} />
+
       <Input type="email" name="email" placeholder="e-mail" />
       <Input type="password" name="password" placeholder="Password" />
+
       <Button type="submit" value="Wyślij">
         Log in
       </Button>
