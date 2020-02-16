9d09d6a0e9da9d0e5280775c78e86bc4b279fc52
MalgoskaG <ghouga.m@gmail.com>
2020-02-16 13:35:22 +0100

    Input controlls

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import CustomLink from "./styled/CustomLink"
-import Button from "./styled/Button"
-import Input from "./styled/Input"
-import Wrapper from "./styled/Wrapper"
-import Flower from "./FLowerAnimation"
-
+import CustomLink from "./styled/CustomLink";
+import Button from "./styled/Button";
+import Input from "./styled/Input";
+import Wrapper from "./styled/Wrapper";
+import Flower from "./FLowerAnimation";
 
 const WrapperLogin = styled(Wrapper)`
   display: flex;
@@ -20,13 +19,49 @@ const WrapperLogin = styled(Wrapper)`
   }
 `;
 
+const onFocus = setFocus => {
+  setFocus(true);
+};
+
+const onBlur = setFocus => {
+  setFocus(false);
+};
+
 const LoginForm = () => {
+  const [focus, setFocus] = useState(false);
+  const [inputValue, setInputValue] = React.useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    const value = e.target.value;
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: value
+    });
+  };
+
   return (
     <WrapperLogin>
-      <Flower/>
+      <Flower focused={focus} />
 
-      <Input type="email" name="email" placeholder="e-mail" />
-      <Input type="password" name="password" placeholder="Password" />
+      <Input
+        type="email"
+        name="email"
+        placeholder="e-mail"
+        onFocus={() => onFocus(setFocus)}
+        onBlur={() => onBlur(setFocus)}
+        value={inputValue.email}
+        onChange={handleChange}
+      />
+      <Input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={inputValue.password}
+        onChange={handleChange}
+      />
 
       <Button type="submit" value="Log in">
         Log in
