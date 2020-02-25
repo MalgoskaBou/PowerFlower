ea5b6f9b0b53415697c452f747d5d94c1608cee0
malgoskaG <malgorzata.ghouga@pronos.eu>
2020-02-25 15:12:42 +0100

    Refactor queriesTestHelper

--- server/__tests__/integration/query.test.js
+++ server/__tests__/integration/query.test.js
@@ -1,7 +1,8 @@
 const request = require("supertest");
 const {
   queryLogIn,
-  querySignUp
+  querySignUp,
+  userData
 } = require("../../testHelpers/queriesTestHelper");
 
 let server;
@@ -27,7 +28,7 @@ const exec = async query => {
     });
 };
 
-describe("GraphQL queries", () => {
+describe("GraphQL queries for user", () => {
   it("User is not logged in", async () => {
     const res = await request(server)
       .post("/graphql")
@@ -38,7 +39,8 @@ describe("GraphQL queries", () => {
   });
 
   it("Login nonexisting user", async () => {
-    const res = await exec(queryLogIn);
+    const query = queryLogIn();
+    const res = await exec(query);
 
     expect(res.status).toBe(200);
     expect(res.text).toMatch(/errors/);
@@ -46,17 +48,30 @@ describe("GraphQL queries", () => {
   });
 
   it("Signup user correctly", async () => {
-    const res = await exec(querySignUp);
+    const query = querySignUp();
+    const res = await exec(query);
 
     expect(res).toHaveProperty("body.data.signupUser");
-    expect(res.body.data.signupUser.email).toMatch("email5@email.com");
+    expect(res.body.data.signupUser.email).toMatch(userData.userEmail);
+  });
+
+  it("Signup user without email", async () => {
+    const email = "";
+    const query = querySignUp(email);
+    const res = await exec(query);
+
+    expect(res.text).toMatch(/errors/);
+    expect(res.text).toMatch(/\\\"email\\\" is not allowed to be empty/);
   });
 
   it("Login user correctly", async () => {
-    await exec(querySignUp);
-    const res = await exec(queryLogIn);
+    const queryForSignUp = querySignUp();
+    const queryForLogIn = queryLogIn();
+
+    await exec(queryForSignUp);
+    const res = await exec(queryForLogIn);
 
     expect(res).toHaveProperty("body.data.loginUser");
-    expect(res.body.data.loginUser.email).toMatch("email5@email.com");
+    expect(res.body.data.loginUser.email).toMatch(userData.userEmail);
   });
 });
--- server/testHelpers/queriesTestHelper.js
+++ server/testHelpers/queriesTestHelper.js
@@ -1,16 +1,24 @@
-const querySignUp = `mutation {
-          signupUser(email:"email5@email.com", password: "dupa", name: "kitek"){
+const userData = {
+  userEmail: "email5@email.com",
+  userPassword: "haslo",
+  userName: "Kitek"
+};
+
+const {userEmail, userPassword, userName} = userData
+
+const querySignUp = (email = userEmail, password = userPassword, name = userName) => (`mutation {
+          signupUser(email:"${email}", password: "${password}", name: "${name}"){
             email
             name
             id
           }
-        }`;
+        }`);
 
-const queryLogIn = `mutation {
-          loginUser(email:"email5@email.com", password: "dupa"){
+const queryLogIn = (email = userEmail, password = userPassword) => (`mutation {
+          loginUser(email:"${email}", password: "${password}"){
             email
             name
           }
-        }`;
+        }`);
 
-module.exports = { querySignUp, queryLogIn };
+module.exports = { querySignUp, queryLogIn, userData };
