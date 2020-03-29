bec0b9f218cd76dfa07f888b3f1b2fc7bb350320
MalgoskaBou <ghouga.m@gmail.com>
2020-03-29 15:19:29 +0200

    Correct problem with background size

--- client/src/components/general-style/Wrapper.js
+++ client/src/components/general-style/Wrapper.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export default styled.section`
-  height: 100vh;
-  width: 100wh;
+  min-height: 100vh;
   background: linear-gradient(125.61deg, #3f5169 0.46%, #202e45 100%);
 `;
